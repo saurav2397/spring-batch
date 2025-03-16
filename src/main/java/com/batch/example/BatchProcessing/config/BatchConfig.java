1d4996f53f57499267d1e5272371aefefc4f4ffe
saurav2397 <sauravmahapure23@gmail.com>
2025-03-16 20:38:51 +0530

    added comments.

--- src/main/java/com/batch/example/BatchProcessing/config/BatchConfig.java
+++ src/main/java/com/batch/example/BatchProcessing/config/BatchConfig.java
@@ -4,6 +4,7 @@ import com.batch.example.BatchProcessing.listener.JobCompletionNotification;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ public class BatchConfig {
     @Bean
     public Job jobBean(JobRepository jobRepository, JobCompletionNotification lister, Step steps){
         return new JobBuilder("initialJob",jobRepository)
+                .incrementer(new RunIdIncrementer())// when job has to execute multiple times, otherwise gives error, fetches data from job repository
                 .listener(lister)
                 .start(steps)
                 .build();
